0105cd7cefd692e6df69fadc6655d1cb4acecccc
sanjaraiy <deepakrai78788@gmail.com>
2024-04-30 23:32:41 +0530

    added mongodb connection using sperate file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-import mongoose from 'mongoose';
-import {DB_NAME} from './constants'
-import express from 'express'
+// require('dotenv').config({path:"./env"})
 
-const app = express();
+import express from 'express';
+import dotenv from "dotenv";
+import connectDB from './db/connection.js';
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
+
+
+
+
+
+
 
 //IIF
-;(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error",(error)=>{
-            console.log('ERROR:',error);
-            throw error;
-        })
+// ;(async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log('ERROR:',error);
+//             throw error;
+//         })
 
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
 
-    } catch (error) {
-        console.error("ERROR: ",error);
-        // throw error
-        // process.exit(1);
-    }
-})()
+//     } catch (error) {
+//         console.error("ERROR: ",error);
+//         // throw error
+//         // process.exit(1);
+//     }
+// })()
